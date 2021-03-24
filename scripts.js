178381e33f1be83fb2e4ae5801f31d011abebbc9
Manav Misra <manavm@visionwebsoft.com>
2021-03-24 13:45:08 -0500

    Add TODO for form submission

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("form");
+const students = [];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   console.log("hello submit");
+
+  // TODO: Get the names from the form and add them to `students`
 });
