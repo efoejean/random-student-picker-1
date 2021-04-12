a27811032f6b3ca7684a028b2608d6c5cf1fe927
Manav Misra <manavm@visionwebsoft.com>
2021-04-12 13:07:09 -0500

    Add comments for clarification

--- scripts.js
+++ scripts.js
@@ -4,15 +4,22 @@ const students = [];
 
 document.body.appendChild(p);
 
+// Copied from MDN
 function getRandomIntInclusive(min, max) {
   const currentMin = Math.ceil(min);
   const currentMax = Math.floor(max);
   return Math.floor(Math.random() * (currentMax - currentMin + 1) + currentMin); // The maximum is inclusive and the minimum is inclusive
 }
 
+// "submit" is on the form NOT on a submit button
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  /**
+   * 1. event.target is the form that got submitted
+   * 2. FormData uses values and name attributes from the form fields
+   * 3. Create a new object from the entries in the form
+   */
   const newStudent = Object.fromEntries(new FormData(event.target));
 
   students.push(newStudent);
